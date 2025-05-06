90c65755ca4153b2891b848997b6f1c92fa605e6
Mariana Bongiolo <MarianaBongiolo1@gmail.com>
2025-05-06 11:05:27 -0400

    Livro.java

--- src/main/java/Livro.java
+++ src/main/java/Livro.java
@@ -1,3 +1,42 @@
 public class Livro {
+    //ATRIBUTOS
+    private static int proximoIsbn = 1;
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private int ano;
     
+    //CONTRUTOR
+    public Livro(String titulo, String autor, int ano){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.isbn=proximoIsbn++;
+    }
+    
+    //METODOS getter
+    public String getTitulo(){
+        return this.titulo;
+    }
+    
+    public String getAutor(){
+        return this.autor;
+    }
+    
+    public int getAno(){
+        return this.ano;
+    }
+    
+    //MEODOS SETTER
+    public void setTitulo(String novoTitulo){
+        this.titulo = novoTitulo;
+    }
+    
+    public void setAutor(String novoAutor){
+        this.autor = novoAutor;
+    }
+    
+    public void setAno(int novoAno){
+        this.ano = novoAno;
+    }
 }
